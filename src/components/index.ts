4eab8768affbd380f9ba63bfbf9e55fbb2249514
cCMartinF <145108625+cCMartinF@users.noreply.github.com>
2024-03-22 12:41:02 +0100

    Added Navbar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { CourseMenu } from "./CourseMenu";
 export { CocktailMenu } from "./CocktailMenu";
 export { SidesMenu } from "./SidesMenu";
+export { NavBarComponent } from "./NavBarComponent";
