1f79ee332dea0ed02b0b7b117e136a2d0a057dd5
anjey11 <david.andhall@hotmail.com>
2024-03-25 15:20:06 +0100

    pass

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,10 @@ const ResetStyles = createGlobalStyle`
 `;
 
 const MainComponent = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center; /* Centrerar innehållet horisontellt */
-padding-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Centrerar innehållet horisontellt */
+  padding-top: 50px;
 `;
 
 const App: React.FC = () => {
