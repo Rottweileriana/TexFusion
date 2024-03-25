efe59bf0f81e5ba314ee6b7454b0f17699b21b4c
cCMartinF <145108625+cCMartinF@users.noreply.github.com>
2024-03-25 12:41:01 +0100

    Added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,47 +6,25 @@ import "./App.css";
 
 const App: React.FC = () => {
 
-  const CourseMenuRef = useRef(null);
-  const SidesMenuRef = useRef(null);
-  const CocktailMenuRef = useRef(null);
-
-  const scrollToRef = (ref) => {
-    switch (ref) {
-      case 'CourseMenu':
-        CourseMenuRef.current.scrollIntoView({ behavior: 'smooth' });
-        break;
-      case 'SidesMenu':
-        SidesMenuRef.current.scrollIntoView({ behavior: 'smooth' });
-        break;
-      case 'CocktailMenu':
-        CocktailMenuRef.current.scrollIntoView({ behavior: 'smooth' });
-        break;
-      case 'ShoppingCart':
-        contactRef.current.scrollIntoView({ behavior: 'smooth' });
-        break;
-      default:
-        break;
-    }
-  };
 
   return (
     <>
       <CartProvider>
-        <div>
-          <NavBarComponent scrollToRef={scrollToRef}/>
-          <div ref={CourseMenuRef}>
-            <CourseMenu  />
-          </div>
-          <div ref={SidesMenuRef}>
-            <SidesMenu />
-          </div>
-          <div ref={CocktailMenuRef}>
-            <CocktailMenu />
-          </div>
+        <NavBarComponent />
           <div>
-          <ShoppingCart />
+            <div id="CourseMenu">
+             <CourseMenu  />
+            </div>
+            <div id="SidesMenu">
+             <SidesMenu />
+            </div>
+            <div id="CocktailMenu">
+             <CocktailMenu />
+            </div>
+            <div id="ShoppingCart">
+             <ShoppingCart />
+            </div>
           </div>
-        </div>
       </CartProvider>
     </>
   );
