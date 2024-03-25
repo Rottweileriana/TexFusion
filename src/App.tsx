70dc19730c1e6ce09342db514c1cdb6da1ebe053
Matthias <widmer.matt@gmail.com>
2024-03-25 10:48:18 +0100

    Simple form implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
-import { CartProvider } from './components/context';
-import { CourseMenu, CocktailMenu, SidesMenu, NavBarComponent, ShoppingCart } from "./components/index";
-import React, {useRef} from 'react'
+import { CartProvider } from "./components/context";
+import {
+  CourseMenu,
+  CocktailMenu,
+  SidesMenu,
+  NavBarComponent,
+  ShoppingCart,
+  CheckoutForm,
+} from "./components/index";
+import React, { useRef } from "react";
 import "./App.css";
 
-
 const App: React.FC = () => {
-
   const CourseMenuRef = useRef(null);
   const SidesMenuRef = useRef(null);
   const CocktailMenuRef = useRef(null);
 
   const scrollToRef = (ref) => {
     switch (ref) {
-      case 'CourseMenu':
-        CourseMenuRef.current.scrollIntoView({ behavior: 'smooth' });
+      case "CourseMenu":
+        CourseMenuRef.current.scrollIntoView({ behavior: "smooth" });
         break;
-      case 'SidesMenu':
-        SidesMenuRef.current.scrollIntoView({ behavior: 'smooth' });
+      case "SidesMenu":
+        SidesMenuRef.current.scrollIntoView({ behavior: "smooth" });
         break;
-      case 'CocktailMenu':
-        CocktailMenuRef.current.scrollIntoView({ behavior: 'smooth' });
+      case "CocktailMenu":
+        CocktailMenuRef.current.scrollIntoView({ behavior: "smooth" });
         break;
-      case 'ShoppingCart':
-        contactRef.current.scrollIntoView({ behavior: 'smooth' });
+      case "ShoppingCart":
+        contactRef.current.scrollIntoView({ behavior: "smooth" });
         break;
       default:
         break;
@@ -33,9 +38,9 @@ const App: React.FC = () => {
     <>
       <CartProvider>
         <div>
-          <NavBarComponent scrollToRef={scrollToRef}/>
+          <NavBarComponent scrollToRef={scrollToRef} />
           <div ref={CourseMenuRef}>
-            <CourseMenu  />
+            <CourseMenu />
           </div>
           <div ref={SidesMenuRef}>
             <SidesMenu />
@@ -44,12 +49,15 @@ const App: React.FC = () => {
             <CocktailMenu />
           </div>
           <div>
-          <ShoppingCart />
+            <ShoppingCart />
+          </div>
+          <div>
+            <CheckoutForm />
           </div>
         </div>
       </CartProvider>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/CheckoutForm.tsx
@@ -0,0 +1,13 @@
+export function CheckoutForm() {
+  return (
+    <div>
+      <h2>KASSA</h2>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+      </form>
+    </div>
+  );
+}
