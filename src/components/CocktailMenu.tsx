33027ca1b4e4f9f9942a086203cd499496e46535
anjey11 <david.andhall@hotmail.com>
2024-04-08 14:43:04 +0200

    implementation of the right five cocktails

--- src/components/CocktailMenu.tsx
+++ src/components/CocktailMenu.tsx
@@ -22,38 +22,49 @@ const MenuTitle = styled.h2`
 
 export function CocktailMenu() {
   const [cocktails, setCocktails] = useState<Cocktail[]>([]);
-  const [cocktailPrice, setCocktailPrice] = useState<number>(0);
-  const cocktailsToFetch: CocktailToFetch[] = [
-    {id: 11007, price: 175},
-    {id: 178365, price: 159},
-    {id: 13621, price: 165},
-    {id: 11003, price: 170},
-    {id: 11001, price: 150},
- ];
-
- // const API_URL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktail.id}`;
 
   useEffect(() => {
+
+    const cocktailsToFetch: CocktailToFetch[] = [
+      {id: 11007, price: 175},
+      {id: 178365, price: 159},
+      {id: 13621, price: 165},
+      {id: 11003, price: 170},
+      {id: 11001, price: 150},
+   ];
 
       const fetchCocktails = async () => {
         try {
 
-          cocktailsToFetch.forEach(async cocktail => {
+          const fetchPromises: Promise<Response>[] = cocktailsToFetch.map(cocktail =>
+            fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktail.id}`)
+          );
 
-            const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktail.id}`);
+          const responses = await Promise.all(fetchPromises);
+          const fetchedCocktails: Cocktail[] = [];
+
+          for (const response of responses) {
             if (!response.ok) {
               throw new Error("Network response was not ok");
             }
             const data = await response.json();
             if (data && data.drinks && data.drinks.length > 0) {
-              setCocktails(data.drinks[0]);
+              fetchedCocktails.push(data.drinks[0]);
             }
-          });
-        } 
-        catch (error) {
+          }
+
+        // Loopar igenom varje cocktail och tilldela cocktailPrice från cocktailsToFetch
+        const cocktailsWithPrices = fetchedCocktails.map(
+          (cocktail: Cocktail, index: number) => ({
+            ...cocktail,
+            cocktailPrice: cocktailsToFetch[index].price,
+          })
+        );
+        setCocktails(cocktailsWithPrices);
+
+        } catch (error) {
           console.error("Error fetching cocktails:", error);
         }
-      
     };
 
       fetchCocktails();
@@ -63,41 +74,6 @@ export function CocktailMenu() {
     };
   }, []);
 
-
-  
-
-  // const API_URL_1 =
-  //   "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic";
-
-  // useEffect(() => {
-  //   const fetchCocktails = async () => {
-  //     try {
-  //       const response = await fetch(API_URL);
-  //       if (!response.ok) {
-  //         throw new Error("Network response was not ok");
-  //       }
-  //       const data = await response.json();
-  //       const firstTenCocktails = data.drinks ? data.drinks.slice(0, 10) : [];
-  //       // Loopar igenom varje cocktail och tilldela cocktailPrice från cocktailPrices
-  //       const cocktailsWithPrices = firstTenCocktails.map(
-  //         (cocktail: Cocktail, index: number) => ({
-  //           ...cocktail,
-  //           cocktailPrice: cocktailPrices[index],
-  //         })
-  //       );
-  //       setCocktails(cocktailsWithPrices);
-  //     } catch (error) {
-  //       console.error("Error fetching cocktails:", error);
-  //     }
-  //   };
-
-  //   fetchCocktails();
-
-  //   return () => {
-  //     setCocktails([]);
-  //     setCocktailPrices([]);
-  //   };
-  // }, []);
 
   return (
     <>
