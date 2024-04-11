aa43f05b166e87567c8380604227a97e3ed8cb88
anjey11 <david.andhall@hotmail.com>
2024-04-11 10:10:12 +0200

    Created About and contact components

---
+++ src/components/AboutComponent.tsx
@@ -0,0 +1,17 @@
+function about () {
+    return (
+        <>
+            <h4>TexFusion</h4>
+            <p>TexFusion dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat. Duis aute irure dolor in reprehenderit
+                in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est 2024.</p>
+        </>
+    )
+}
+
+export default about;
