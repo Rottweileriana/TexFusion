15c940396c728d37a8b135090d6b0142cbbb5b40
Matthias <widmer.matt@gmail.com>
2024-05-02 15:23:20 +0200

    Fix background images

--- src/components/AboutComponent.tsx
+++ src/components/AboutComponent.tsx
@@ -1,25 +1,26 @@
 import styled from "styled-components";
 
 const BackgroundImageContainer = styled.div`
-    position: relative;
-    display: flex;
-    justify-content: center;
-    width: 100vw;
-    margin: 0;
-    padding: 0;
-    min-height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
 
-    &::before {
+  &::before {
     content: "";
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     background-image: url("./images/StreetMexicoBackground6.jpg");
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
+    background-attachment: fixed;
     filter: blur(4px);
     transform: scale(1.02);
     z-index: -1;
@@ -27,43 +28,44 @@ const BackgroundImageContainer = styled.div`
 `;
 
 const AboutUsContainer = styled.div`
-    width: 800px;
-    height: 240px;
-    margin: 229px 0 0 0;
-    padding: 0 15px 0 15px;
-    text-align: center;
-    color: #333333;
-    background-color: rgba(245, 245, 245, 0.5);
+  width: 800px;
+  height: 240px;
+  margin: 229px 0 0 0;
+  padding: 0 15px 0 15px;
+  text-align: center;
+  color: #333333;
+  background-color: rgba(245, 245, 245, 0.5);
 `;
 
 const Title = styled.h3`
-    font-family: "Open Sans";
-    font-weight: 400;
-    font-size: 30px;
-    color: #333333;
+  font-family: "Open Sans";
+  font-weight: 400;
+  font-size: 30px;
+  color: #333333;
 `;
 
 const Text = styled.p`
-    text-align: justify;
-    font-weight: 400;
+  text-align: justify;
+  font-weight: 400;
 `;
 
-export function AboutComponent () {
-    return (
-        <>
-            <BackgroundImageContainer>
-                <AboutUsContainer>
-                <Title>OM OSS</Title>
-                    <Text>TexFusion dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit
-                        in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est 2024.</Text>
-                </AboutUsContainer>
-            </BackgroundImageContainer>
-        </>
-    )
+export function AboutComponent() {
+  return (
+    <>
+      <BackgroundImageContainer>
+        <AboutUsContainer>
+          <Title>OM OSS</Title>
+          <Text>
+            TexFusion dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est 2024.
+          </Text>
+        </AboutUsContainer>
+      </BackgroundImageContainer>
+    </>
+  );
 }
